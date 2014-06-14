2c29b21efe76aea392f4a062b49ddb25fc493585
hemstoc <gary.hemstock@gmail.com>
2014-06-13 21:00:40 -0400

    Updated files

--- public_html/createuser.php
+++ public_html/createuser.php
@@ -1,15 +1,35 @@
 <?php
 	require_once('lib/lib.php');
 
-	output_html5_header(
-		'New User',
-		array("css/common.php")
-	);
+	if(!empty($_POST))
+	{
+		output_html5_header('test');
+		output_page_header();
+		output_page_menu();
+		
+		foreach($_POST as $key => $value)
+		{
+			echo<<<ZZEOF
+<p>$key = $value</p>
+ZZEOF;
+		}
 
-	output_page_header();
-	output_page_menu();
-	output_createuser_page_content();
-	output_page_footer();
+		output_page_footer();
 
-	output_html5_footer();
+		output_html5_footer();
+	}
+	else
+	{
+		output_html5_header(
+			'New User',
+			array("css/common.php")
+		);
+
+		output_page_header();
+		output_page_menu();
+		output_createuser_page_content();
+		output_page_footer();
+
+		output_html5_footer();
+	}
 ?>
--- public_html/lib/lib.php
+++ public_html/lib/lib.php
@@ -80,30 +80,66 @@ ZZEOF;
 
 function output_post_page_content()
 {
-	echo<<<ZZEOF
-<div id="content">Post page content!</div>
+	if(is_user_logged_in())
+	{
+		echo<<<ZZEOF
+<form action='post.php' id="content" method='post' accept-charset='UTF-8'>
+	<fieldset>
+		<legend>New Post</legend>
+		<label for='cardname'>Card Name:</label>
+		<input type='text' name='cardname' maxlength="50"/>
+		<br />
+		<label for='cardset'>Card Set:</label>
+		<input type='text' name='cardset' maxlength="50"/>
+		<br />
+		<label for='condition'>Condition:</label>
+		<select name="condition">
+			<option value=""></option>
+			<option value="nm">Near Mint</option>
+			<option value="sp">Lightly Played</option>
+			<option value="mp">Moderately Played</option>
+			<option value="hp">Heavily Played</option>
+			<option value="d">Damaged</option>
+		</select>
+		<br />
+		<input type="radio" name="r" value="has">To Trade</input>
+		<input type="radio" name="r" value="wants">Looking For</input>
+		<br />
+		<label for="file">Filename:</label>
+		<input type="file" name="file" id="file">
+		<br />
+		<input type='submit' name='Submit' value='Submit'/>
+	</fieldset>
+</form>
 ZZEOF;
+	}
+	else
+	{
+		echo<<<ZZEOF
+<div id="content">Please login to post</div>
+ZZEOF;
+	}
 }
 
 function output_createuser_page_content()
 {
 	echo<<<ZZEOF
-<form id="content" method='post' accept-charset='UTF-8'>
+<form action='createuser.php' id="content" method='post' accept-charset='UTF-8'>
 	<fieldset>
 		<legend>New User</legend>
 		<label for='username'>Username:</label>
-		<input type='text' name='username' id='username' maxlength="50"/>
+		<input type='text' name='username' maxlength="50"/>
 		<br />
 		<label for='email'>Email:</label>
-		<input type='text' name='email' id='email' maxlength="50"/>
+		<input type='text' name='email' maxlength="50"/>
 		<br />
 		<label for='password1'>Password:</label>
-		<input type='password' name='password1' id='password' maxlength="50"/>
+		<input type='password' name='password1' maxlength="50"/>
 		<br />
 		<label for='password2'>Re-enter:</label>
-		<input type='password' name='password2' id='password' maxlength="50"/>
+		<input type='password' name='password2' maxlength="50"/>
 		<br />
-		<input action='createuser.php' type='submit' name='NewUser' value='New User'/>
+		<input type='submit' name='NewUser' value='New User'/>
 	</fieldset>
 </form>
 ZZEOF;
@@ -124,10 +160,10 @@ ZZEOF;
 	<fieldset>
 		<legend>Login</legend>
 		<label for='username'>Username:</label>
-		<input type='text' name='username' id='username' maxlength="50"/>
+		<input type='text' name='username' maxlength="50"/>
 		<br />
 		<label for='password'>Password:</label>
-		<input type='password' name='password' id='password' maxlength="50"/>
+		<input type='password' name='password' maxlength="50"/>
 		<br />
 		<input type='submit' name='Submit' value='Submit'/>
 		<input type='submit' name='NewUser' value='New User'/>
